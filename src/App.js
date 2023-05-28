82b4f21d54270b4281769597f072ba5c1cfc945e
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2023-05-29 00:16:37 +0100

    initial hero banner

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "./components/Hero";
+
+
+
+
+export default function App(){
+
+    return(
+        <>
+        <Hero />
+        
+        </>
+
+    )
+}
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style.css";
+import heroImage from "../images/Hero.jpg"
+
+export default function Hero(){
+    return( 
+        <>
+             <img src={heroImage} alt="Hero image" />
+        </>
+
+    );
+
+}
